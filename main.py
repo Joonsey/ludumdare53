175880091cfebf0958e7e951c1b8473bbe432d1f
johannes <johannes.ronning@tietoevry.com>
2023-04-29 07:32:00 +0200

    added Vec2 class and some player input handling

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from pprint import pprint
 from typing import Protocol
 
 import pygame
+import sys
 import enum
 
 DISPLAY_DIMESION = (1080, 720)
@@ -30,9 +31,66 @@ class FloorBehaviour(Behaviour):
 class ConveyorBehaviour(Behaviour):
     pass
 
+class Vec2:
+    def __init__(self, x: int | float, y: int | float) -> None:
+        self.x = x
+        self.y = y
+
+    @classmethod
+    def from_tuple(cls, pos: tuple[int | float, int | float]) -> Vec2:
+        return cls(pos[0], pos[1])
+
+    def __add__(self, other: Vec2) -> Vec2:
+        return Vec2(other.x + self.x, other.y + self.y)
+
+    def __repr__(self) -> str:
+        return f"x: {self.x}, y: {self.y}"
+
 class Postman:
-    def __init__(self, pos: tuple[int, int]) -> None:
-        self.pos = pos
+    def __init__(self, pos: tuple[int | float, int | float]) -> None:
+        self.pos: Vec2 = Vec2.from_tuple(pos)
+        self.velocity: Vec2 = Vec2(0,0)
+        self.acceleration = 0
+        self.max_velocity = 12
+        self.base_acceleration = 4
+        self.speed = 20
+
+    def update(self, dt: float) -> None:
+        delta = self._handle_inputs(dt)
+
+        #self.pos.x = self.pos.x + delta.x
+        #self.pos.y = self.pos.y + delta.y
+        self.pos = self.pos + delta
+
+
+    def _handle_inputs(self, dt: float) -> Vec2:
+        keys = pygame.key.get_pressed()
+        normalized_dt = dt/1000
+
+        if keys[pygame.K_w]:
+            self.velocity.y =- self.acceleration * normalized_dt
+
+        elif keys[pygame.K_s]:
+            self.velocity.y =+ self.acceleration * normalized_dt
+        else:
+            self.velocity.y = 0
+
+        if keys[pygame.K_a]:
+            self.velocity.x =- self.acceleration * normalized_dt
+
+        elif keys[pygame.K_d]:
+            self.velocity.x =+ self.acceleration * normalized_dt
+        else:
+            self.velocity.x = 0
+
+        if self.velocity.x or self.velocity.y:
+            self.acceleration *= dt / 10
+            if self.acceleration >= self.max_velocity:
+                self.acceleration = self.max_velocity
+        else:
+            self.acceleration = self.base_acceleration / 4
+
+        return Vec2(self.velocity.x * self.speed * dt, self.velocity.y * self.speed * dt)
 
 class Level(enum.IntEnum):
     test  = 0
@@ -72,6 +130,9 @@ class Office:
             for x, tile in enumerate(self._map_data[y]):
                 surf.blit(tile.surf, (x * self.size, y * self.size))
 
+    def update(self) -> None:
+        ...
+
     def get_player(self) -> Postman:
         assert isinstance(self._player, Postman), "player not initialized during map generation!"
         return self._player
@@ -82,12 +143,10 @@ class Office:
             for line in file.readlines():
                 line = line.replace('\n','')
                 row = line.split(',')
-                assert len(row) == MAP_WIDTH
+                assert len(row) == MAP_WIDTH, "map data is not of correct size"
                 data.append(row)
 
         return data
-
-
 
 class Package:
     ...
@@ -139,7 +198,11 @@ class Game:
 
     def run(self):
         while self.running:
+            self.deltatime = self.clock.tick(FPS)
+            self._check_should_close()
 
+            self.office.update()
+            self.player.update(self.deltatime)
             self.office.render(self.surf)
             self._draw_surface_on_display()
 
@@ -152,6 +215,12 @@ class Game:
         pygame.display.flip()
         self.display.fill(0)
         self.surf.fill(0)
+
+    def _check_should_close(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
 
 if __name__ == "__main__":
     game = Game(DISPLAY_DIMESION)
